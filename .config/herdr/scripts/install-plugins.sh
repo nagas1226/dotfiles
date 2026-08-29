d1dfebe2dff8c273ae745e706c00a121e5415338
nagas1226 <nagasyo.1226@gmail.com>
2026-08-29 13:14:30 +0900

    feat: add herdr and aerospace config

---
+++ .config/herdr/scripts/install-plugins.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eu
+
+herdr_bin=${HERDR_BIN_PATH:-herdr}
+
+# Install the exact source revisions pinned by this dotfiles setup.
+"$herdr_bin" plugin install cedrus-8864/herdr-prompt-reply \
+  --ref e34e7d0040d910dac0218745462a7e0e4cc66f1b \
+  --yes
+
+"$herdr_bin" plugin install qu8n/herdr-automatic-rename \
+  --ref 11cbee3aa54ea3a507550e4f58a2727de2b119eb \
+  --yes
+
+"$herdr_bin" plugin install zackshen/herdr-workspace \
+  --ref b5f38d0e295168fcd87284b710049a183140f010 \
+  --yes
+
+"$herdr_bin" plugin install yoshiori/herdr-configurable-picker \
+  --ref 9b53d836f226d51efc81a70185db08028bd574c1 \
+  --yes
+
+"$herdr_bin" plugin install JonasBaeumer/herdr-file-annotator \
+  --ref ac9b5efc6e62b276ee351c5dbd6536e6d90df152 \
+  --yes
